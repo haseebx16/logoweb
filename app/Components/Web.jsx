daf17ee0008d2438ffe9ed9619d5dbf3a1276ea8
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-25 00:01:14 +0500

    s

---
+++ app/Components/Web.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { font } from './font/font'
+
+const Web = () => {
+  return (
+    <div className={`${font.className} bg-background text-white`}>
+
+    </div>
+  )
+}
+
+export default Web
