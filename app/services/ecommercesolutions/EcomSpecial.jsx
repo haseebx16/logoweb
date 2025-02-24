ce2565ba61312376812e0e850ecf0132d2bb22eb
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-25 03:17:05 +0500

    Update EcomSpecial.jsx

--- app/services/ecommercesolutions/EcomSpecial.jsx
+++ app/services/ecommercesolutions/EcomSpecial.jsx
@@ -4,31 +4,32 @@ import { font } from '@/app/Components/font/font'
 const EcomSpecial = () => {
   return (
     <div className={`${font.className} flex bg-background flex-col justify-center items-center p-12`}>
-            <h1 className='md:text-5xl text-white text-2xl font-bold text-center'>Services We Offer</h1>
+            <h1 className='md:text-5xl text-white text-2xl font-bold text-center'>Services</h1>
             <div className='grid md:grid-cols-3 grid-cols-1 mt-8 gap-8'>
                 <div className='p-12 flex hover:bg-white text-white hover:text-black duration-300 flex-col justify-center items-center border-2 border-zinc-600 rounded-sm'>
-                    <h1 className='text-cyan-300 text-center text-3xl'>Responsiveness</h1>
-                    <p className='text-center pt-2'>Your brand's exposure may increase by using responsive websites on several platforms.</p>
+                    <h1 className='text-cyan-300 text-center text-3xl'>Mobile Compatible</h1>
+                    <p className='text-center pt-2'>Make your e-commerce website responsive to mobile, tablet, and desktop devices. Our team creates responsive and user-friendly e-commerce websites for new and existing businesses.</p>
                 </div>
                 <div className='p-12 flex hover:bg-white text-white hover:text-black duration-300 flex-col justify-center items-center border-2 border-zinc-600 rounded-sm'>
-                    <h1 className='text-cyan-300 text-3xl'>Unique Design</h1>
-                    <p className='text-center pt-2'>We build our online apps in a way that encourages lead conversion.</p>
+                    <h1 className='text-cyan-300 text-center text-3xl'>Logistics Integration</h1>
+                    <p className='text-center pt-2'>Our e-commerce website enables businesses to connect logistic operations, allowing you to focus on sales rather than integration complexity.</p>
                 </div>
                 <div className='p-12 flex hover:bg-white text-white hover:text-black duration-300 flex-col justify-center items-center border-2 border-zinc-600 rounded-sm'>
-                    <h1 className='text-cyan-300 text-center text-3xl'>SEO</h1>
-                    <p className='text-center pt-2'>Professionals at Website Bravo provide optimized and keyword-rich web apps for our clients.</p>
+                    <h1 className='text-cyan-300 text-center text-3xl'>Check-Out Features</h1>
+                    <p className='text-center pt-2'>Our team offers convenient check-out alternatives. Our user-friendly and responsive technology makes it simple for your customers to check out.</p>
                 </div>
                 <div className='p-12 flex hover:bg-white text-white hover:text-black duration-300 flex-col justify-center items-center border-2 border-zinc-600 rounded-sm'>
-                    <h1 className='text-cyan-300 text-center text-3xl'>Security</h1>
-                    <p className='text-center pt-2'>Our company develops secure and SSL-certified web applications.</p>
+                    <h1 className='text-cyan-300 text-center text-3xl'>Payment Gateway</h1>
+                    <p className='text-center pt-2'>Our e-commerce website designers use impenetrable payment modules to establish secure payment gateways. Integrate 50+ payment methods to reach the maximum number of clients.</p>
                 </div>
                 <div className='p-12 flex hover:bg-white text-white hover:text-black duration-300 flex-col justify-center items-center border-2 border-zinc-600 rounded-sm'>
-                    <h1 className='text-cyan-300 text-center text-3xl'>UI/UX</h1>
-                    <p className='text-center pt-2'>A captivating user experience should be presented effectively for any online application to benefit your business.</p>
+                    <h1 className='text-cyan-300 text-center text-3xl'>Robust Showcasing</h1>
+                    <p className='text-center pt-2'>
+                    Convertible and mobile-ready apps are beneficial for our customers to create.</p>
                 </div>
                 <div className='p-12 flex hover:bg-white text-white hover:text-black duration-300 flex-col justify-center items-center border-2 border-zinc-600 rounded-sm'>
-                    <h1 className='text-cyan-300 text-center text-3xl'>Speed</h1>
-                    <p className='text-center pt-2'>Search engine algorithms prefer websites with quick and simple loading times. Thankfully, our web developers have a wide range of skills to increase web performance.</p>
+                    <h1 className='text-cyan-300 text-center text-3xl'>Shopping Cart</h1>
+                    <p className='text-center pt-2'>Customers can purchase online swiftly thanks to our shopping cart function, which is supported by the most cutting-edge technology.</p>
                 </div>
             </div>
         </div>
