51aacc489fba22e87dbea54f2aae056a532731e7
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-24 23:54:11 +0500

    Update page.jsx

--- app/services/websitedevelopment/page.jsx
+++ app/services/websitedevelopment/page.jsx
@@ -19,7 +19,7 @@ const page = () => {
             Custom Website
             Development
             </h1>
-            <p className='md:text-xl text-sm py-4 text-white'>No matter how big or small, every business can count on Website Bravo as an indispensable ally in its digital expansion. Our expert bespoke website design and development services are carried out with a laser-like concentration on producing quantifiable outcomes. To construct successful enterprises, websites in today's digital age need to be dynamic, engaging, and flexible.</p>
+            <p className='md:text-xl text-sm py-4 text-white'>No matter how big or small, every business can count on LogoWeb USA as an indispensable ally in its digital expansion. Our expert bespoke website design and development services are carried out with a laser-like concentration on producing quantifiable outcomes. To construct successful enterprises, websites in today's digital age need to be dynamic, engaging, and flexible.</p>
             <p className='md:text-xl text-sm py-4 text-white'>Our UI/UX experts have an in-depth understanding of your company's needs, and they work diligently to build an appealing custom website based on your preferences.</p>
             
             <a href='/about'><button className="btn-primary text-cyan-300 text-lg uppercase tracking-wider">
