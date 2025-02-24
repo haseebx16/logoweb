1dd4f9c74ed230f11098883c468a1520a6099dd7
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-25 00:04:34 +0500

    asd

--- app/Components/Partners.jsx
+++ app/Components/Partners.jsx
@@ -3,7 +3,7 @@ import { font } from './font/font'
 
 const Partners = () => {
   return (
-    <div className={`${font.className} flex flex-col md:flex-row space-x-0 md:space-y-0 space-y-4 md:space-x-4 py-4 justify-center items-center p-2 bg-background`}>
+    <div className={`${font.className} flex flex-col md:flex-row space-x-0 md:space-y-0 space-y-4 md:space-x-4 py-4 pb-16 justify-center items-center p-2 bg-background`}>
         <img src='/partner2.webp'/>
         <img src='/partner3.webp'/>
         <img src='/partner7.webp'/>
--- app/Components/Web.jsx
+++ app/Components/Web.jsx
@@ -3,8 +3,8 @@ import { font } from './font/font'
 
 const Web = () => {
   return (
-    <div className={`${font.className} bg-background text-white`}>
-
+    <div className={`${font.className} bg-background container text-white p-20`}>
+        <h1 className='text-2xl md:text-4xl text-center font-extrabold'>Our Custom Web Development Services Process</h1>
     </div>
   )
 }
