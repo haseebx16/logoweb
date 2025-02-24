21783e60ebbaabccbb6256d6c18a5f58a2c19c92
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-24 22:57:36 +0500

    Update OurExpertise.jsx

--- app/Components/OurExpertise.jsx
+++ app/Components/OurExpertise.jsx
@@ -5,11 +5,11 @@ const OurExpertise = () => {
   return (
     <div className={`${font.className} md:p-20 p-8 overflow-hidden space-y-8 md:space-y-0 md:space-x-8 text-white w-full flex justify-between items-center flex-col md:flex-row bg-zinc-900`}>
         <div>
-            <img src='/our-expertise.jpg' className='h-80 w-auto'/>
+            <h1 className='font-extrabold text-5xl'>Our Expertise</h1>
+            <p className='max-w-2xl mt-4'>Our brand experts, web designers, and e-commerce managers with extensive operational expertise in digital performance and marketing. With data analytics, our expertise becomes more evident. Additionally, our team of digital experts uses data as input for the mill to deliver our partners’ brands in front of their intended audience.</p>
         </div>
         <div>
-            <h1 className='font-extrabold text-5xl'>Our Philosophy</h1>
-            <p className='max-w-2xl mt-4'>“It all begins with the right team.” We think big things can happen when you bring together people from different experiences who are strategic, compassionate, enthusiastic, open-minded, and naturally curious. Diversity, cooperation, competition, professional advancement, environmental improvement, and mutual prosperity are encouraged by our brand. Additionally, our digital team goes above and above to remove obstacles on the way to success for our partners. Website Bravo values data-driven financial development, practical work environments, and strategic relationships.</p>
+            <img src='/our-expertise.jpg' className='h-80 w-auto'/>
         </div>
     </div>
   )
