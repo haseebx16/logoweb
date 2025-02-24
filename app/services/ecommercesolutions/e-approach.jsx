a3f31d6b8b965bab4ae826efcb44e9432dc0f9f0
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-25 03:08:10 +0500

    Update e-approach.jsx

--- app/services/ecommercesolutions/e-approach.jsx
+++ app/services/ecommercesolutions/e-approach.jsx
@@ -5,12 +5,12 @@ const EApproach = () => {
   return (
     <div className={`${font.className} md:p-20 p-8 overflow-hidden space-y-8 md:space-y-0 md:space-x-8 text-white w-full flex justify-between items-center flex-col md:flex-row bg-zinc-900`}>
             <div>
-                <img src='/approach.png' className='h-96 w-full'/>
+                <img src='/approach.png' className='h-64 w-full'/>
             </div>
             <div>
-                <h1 className='font-extrabold text-5xl'>
+                <h1 className='font-extrabold text-4xl'>
                 E-Commerce Development Company In New York</h1>
-                <p className='max-w-2xl mt-4'>As part of our e-commerce website development services, LogoWeb USA builds any universal or one-of-a-kind features that clients may need for a smooth journey. In addition, we have the appropriate level of experience to do the task error-free, regardless of your business or technological model.</p>
+                <p className='max-w-xl mt-4'>As part of our e-commerce website development services, LogoWeb USA builds any universal or one-of-a-kind features that clients may need for a smooth journey. In addition, we have the appropriate level of experience to do the task error-free, regardless of your business or technological model.</p>
             </div>
         </div>
   )
