a3900dc32818e4b5d30c8a00dfeca549b714c081
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-25 03:34:26 +0500

    Update AppApproach.jsx

--- app/services/webapplicationdevelopment/AppApproach.jsx
+++ app/services/webapplicationdevelopment/AppApproach.jsx
@@ -5,7 +5,7 @@ const AppApproach = () => {
   return (
     <div className={`${font.className} md:p-20 p-8 overflow-hidden space-y-8 md:space-y-0 md:space-x-8 text-white w-full flex justify-between items-center flex-col md:flex-row bg-zinc-900`}>
             <div>
-                <img src='/approach3.png' className='h-80 w-auto'/>
+                <img src='/approach3.png' className='h-80 w-[100%]'/>
             </div>
             <div>
                 <h1 className='font-extrabold text-5xl'>Custom Web Application Development Company</h1>
