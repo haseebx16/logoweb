2cef8d7e63741d890e17210a5ce6cd3e1675d793
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-26 22:38:50 +0500

    Create WhyChoose.jsx

---
+++ app/services/logodesign/WhyChoose.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { font } from '@/app/Components/font/font'
+
+const WhyChoose = () => {
+  return (
+    <div className={`${font.className} bg-background text-white p-20 flex flex-col justify-center items-center`}>
+        <h1 className='text-3xl font-extrabold md:text-5xl'>Why Choose Us</h1>
+    </div>  
+  )
+}
+
+export default WhyChoose
