ba300a38e02f7aa22aaa2a9d0ed29621135c89c2
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-27 21:40:32 +0500

    Update page.jsx

--- app/technologies/page.jsx
+++ app/technologies/page.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Header from '../Components/Header'
 import { font } from '../Components/font/font'
-import Portfolio from '../Components/Portfolio'
 import Talk from '../Components/Talk'
 import Contact from '../Components/Contact'
 import Footer from '../Components/Footer'
 import { lightFont } from '../Components/font/lightFont'
+import { FaPhp } from "react-icons/fa6";
+import { FaPython } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { FaAngular } from "react-icons/fa";
+import { FaVuejs } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
+import { FaAndroid } from "react-icons/fa";
+import { TbBrandXamarin } from "react-icons/tb";
+import { FaLaravel } from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
 
 const page = () => {
   return (
@@ -33,48 +44,65 @@ const page = () => {
                     <h1 className=' md:text-6xl text-3xl font-extrabold text-center'>We have complete knowledge of these technologies</h1>    
                     </div>
                     <div className='grid mt-8 grid-cols-1 md:grid-cols-3 gap-4'>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                            <FaPhp className='text-7xl text-cyan-700'/>
                             <h2 className='text-2xl font-bold'>PHP</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <FaPython className='text-7xl text-cyan-700'/>
+                            
                             <h2 className='text-2xl font-bold'>Python</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        {/* <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
                             <h2 className='text-2xl font-bold'>.NET</h2>
-                        </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        </div> */}
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        <FaJava className='text-7xl text-cyan-700'/>
+                            
                             <h2 className='text-2xl font-bold'>Java</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <FaNodeJs className='text-7xl text-cyan-700'/>
+
                             <h2 className='text-2xl font-bold'>Node.js</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <FaAngular className='text-7xl text-cyan-700'/>
+
                             <h2 className='text-2xl font-bold'>Angular</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        <FaVuejs className='text-7xl text-cyan-700'/>
+
                             <h2 className='text-2xl font-bold'>Vue.JS</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <FaReact className='text-7xl text-cyan-700'/>
+                            
                             <h2 className='text-2xl font-bold'>React</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                            <FaApple className='text-7xl text-cyan-700'/>
                             <h2 className='text-2xl font-bold'>iOS</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
+                        <FaAndroid className='text-7xl text-cyan-700'/>
+
                             <h2 className='text-2xl font-bold'>Android</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                            <TbBrandXamarin className='text-7xl text-cyan-700'/>
                             <h2 className='text-2xl font-bold'>Xamarin</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                            <FaLaravel className='text-7xl text-cyan-700'/>
                             <h2 className='text-2xl font-bold'>Laravel</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                        <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
+                            <DiDjango className='text-7xl text-cyan-700'/>
                             <h2 className='text-2xl font-extrabold'>Django</h2>
                         </div>
-                        <div className='bg-transparent hover:bg-cyan-400 duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                            <h2 className='text-2xl font-extrabold'>MEAN Stack</h2>
-                        </div>
+                       
 
                     </div>
 
