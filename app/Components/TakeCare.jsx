bc7915889bc892547e094b3b3d232d45706fbbc9
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-01 04:35:20 +0500

    sd

--- app/Components/TakeCare.jsx
+++ app/Components/TakeCare.jsx
@@ -6,7 +6,7 @@ const TakeCare = () => {
     <div className={`${font.className} bg-zinc-900 text-white overflow-hidden flex flex-col items-center justify-center py-20`}>
         <div className='p-20 flex flex-col justify-center items-center'>
             <h1 className='font-extrabold text-center text-2xl md:text-5xl'>To lend expression to our digital brand philosophy, J. Willard Marriott says:</h1>
-            <h1 className='font-extrabold text-cyan-500 text-center mt-8 text-3xl md:text-5xl'>“Take care of your people, and they will take care of your partners.”</h1>
+            <h1 className='font-extrabold text-red-400 text-center mt-8 text-3xl md:text-5xl'>“Take care of your people, and they will take care of your partners.”</h1>
         </div>
         <div className='p-12 pt-0 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 justify-evenly items-center'>
             <div className='bg-zinc-800 w-60 h-60 p-4'>
--- app/technologies/page.jsx
+++ app/technologies/page.jsx
@@ -27,12 +27,12 @@ const page = () => {
                       <div className="flex flex-col md:flex-row items-center justify-center gap-12">
                         <div className="text-white text-center max-w-4xl">
                           
-                          <h1 className="font-oswald text-center text-4xl  md:text-5xl lg:text-6xl font-extrabold mb-6 tracking-wide">
+                          <h1 className="font-oswald text-center text-red-600 text-4xl  md:text-5xl lg:text-6xl font-extrabold mb-6 tracking-wide">
                           Expertise in various cutting-edge technologies
                           </h1>
                           <p className={`${lightFont.className} md:text-xl text-center font-light text-sm py-4 text-white`}>Our work is a testament to our expertise. It speaks aloud in respect of e-commerce, web and mobile application, SEO, Google ad words and email marketing, branding and social media marketing. It demonstrates the caliber of value achieved for businesses like yours.</p>
                           
-                          <a href='/about'><button className="btn-primary text-cyan-300 text-lg uppercase tracking-wider">
+                          <a href='/about'><button className="btn-primary text-red-600 text-lg uppercase tracking-wider">
                             Discover Our Data-Driven Strategies
                           </button></a>
                         </div>
@@ -41,15 +41,15 @@ const page = () => {
                 </section>
                 <section className={` ${font.className} p-20 px-16 flex flex-col text-white justify-center items-center bg-zinc-950`}>
                     <div>
-                    <h1 className=' md:text-6xl text-3xl font-extrabold text-center'>We have complete knowledge of these technologies</h1>    
+                    <h1 className=' md:text-6xl text-3xl text-red-600 font-extrabold text-center'>We have complete knowledge of these technologies</h1>    
                     </div>
                     <div className='grid mt-8 grid-cols-1 md:grid-cols-3 gap-4'>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
-                            <FaPhp className='text-7xl text-cyan-700'/>
+                            <FaPhp className='text-7xl text-red-600'/>
                             <h2 className='text-2xl font-bold'>PHP</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                        <FaPython className='text-7xl text-cyan-700'/>
+                        <FaPython className='text-7xl text-red-600'/>
                             
                             <h2 className='text-2xl font-bold'>Python</h2>
                         </div>
@@ -57,49 +57,49 @@ const page = () => {
                             <h2 className='text-2xl font-bold'>.NET</h2>
                         </div> */}
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
-                        <FaJava className='text-7xl text-cyan-700'/>
+                        <FaJava className='text-7xl text-red-600'/>
                             
                             <h2 className='text-2xl font-bold'>Java</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                        <FaNodeJs className='text-7xl text-cyan-700'/>
+                        <FaNodeJs className='text-7xl text-red-600'/>
 
                             <h2 className='text-2xl font-bold'>Node.js</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                        <FaAngular className='text-7xl text-cyan-700'/>
+                        <FaAngular className='text-7xl text-red-600'/>
 
                             <h2 className='text-2xl font-bold'>Angular</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
-                        <FaVuejs className='text-7xl text-cyan-700'/>
+                        <FaVuejs className='text-7xl text-red-600'/>
 
                             <h2 className='text-2xl font-bold'>Vue.JS</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                        <FaReact className='text-7xl text-cyan-700'/>
+                        <FaReact className='text-7xl text-red-600'/>
                             
                             <h2 className='text-2xl font-bold'>React</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                            <FaApple className='text-7xl text-cyan-700'/>
+                            <FaApple className='text-7xl text-red-600'/>
                             <h2 className='text-2xl font-bold'>iOS</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 flex flex-col justify-center items-center p-12 px-24 border border-zinc-700 '>
-                        <FaAndroid className='text-7xl text-cyan-700'/>
+                        <FaAndroid className='text-7xl text-red-600'/>
 
                             <h2 className='text-2xl font-bold'>Android</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                            <TbBrandXamarin className='text-7xl text-cyan-700'/>
+                            <TbBrandXamarin className='text-7xl text-red-600'/>
                             <h2 className='text-2xl font-bold'>Xamarin</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                            <FaLaravel className='text-7xl text-cyan-700'/>
+                            <FaLaravel className='text-7xl text-red-600'/>
                             <h2 className='text-2xl font-bold'>Laravel</h2>
                         </div>
                         <div className='bg-transparent hover:bg-white hover:text-black duration-700 p-12 flex flex-col justify-center items-center px-24 border border-zinc-700 '>
-                            <DiDjango className='text-7xl text-cyan-700'/>
+                            <DiDjango className='text-7xl text-red-600'/>
                             <h2 className='text-2xl font-extrabold'>Django</h2>
                         </div>
                        
