329985b5ced3593392b4b0b121422864a6e9fa31
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-02-28 22:33:34 +0500

    Update page.jsx

--- app/services/ecommercesolutions/page.jsx
+++ app/services/ecommercesolutions/page.jsx
@@ -17,7 +17,7 @@ const page = () => {
   return (
     <div className={`${font.className} `}>
         <Header/>
-        <section className={` ${font.className} relative h-screen md:h-[50rem] px-20  bg-background`}>
+        <section className={` ${font.className} relative h-screen md:h-[40rem] px-20  bg-background`}>
             <div className="container mx-auto px-4 py-48 relative z-20">
               <div className="flex flex-col md:flex-row items-center justify-between gap-12">
                 <div className="text-white max-w-4xl">
@@ -26,13 +26,12 @@ const page = () => {
                   Ecommerce
                   Solutions
                   </h1>
-                  <p className='md:text-xl text-sm py-4 text-white'>LogoWeb USA assists companies in establishing a dynamic and client-focused e-commerce website to facilitate the expansion of their online operations.
-
-Contact us for e-commerce web development solutions!</p>
+                  <p className='md:text-xl text-sm py-4 text-white'>LogoWeb USA assists companies in establishing a dynamic and client-focused e-commerce website to facilitate the expansion of their online operations.</p>
+                  <p className='md:text-xl text-sm py-4 text-white'>Contact us for e-commerce web development solutions!</p>
                   
-                  <a href='/about'><button className="btn-primary text-cyan-300 text-lg uppercase tracking-wider">
-                    Discover Our Data-Driven Strategies
-                  </button></a>
+                <button className="btn-primary text-white bg-cyan-400 px-8 rounded-md py-4 text-lg uppercase tracking-wider">
+                    Let's Talk
+                  </button>
                 </div>
               </div>
           </div>
