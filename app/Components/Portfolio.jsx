8e0eed69d92128d2acdd7449525a5fef2614a700
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-14 00:20:44 +0500

    Update Portfolio.jsx

--- app/Components/Portfolio.jsx
+++ app/Components/Portfolio.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { font } from "./font/font";
 
 const Portfolio = () => {
-  const [tab, setTab] = useState("Website Development");
+  const [tab, setTab] = useState("Animations");
 
   const tabImages = {
     "Website Development": [
