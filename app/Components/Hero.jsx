96e931d1e3b60aeced6524cc902636f004835738
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-05-02 05:06:26 +0500

    Update Hero.jsx

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className={` ${font.className} relative h-screen md:h-[44rem] px-20  bg-background`}>
 
       {/* Content */}
-      <div className="container mx-auto px-4 py-20 relative z-20" style="margin-top: 96px;">
+      <div className="container mx-auto px-4 py-20 relative z-20" style="margin-top:96px;">
         <div className="flex flex-col md:flex-row items-center justify-between gap-12">
           {/* Text Content */}
           <div className="text-white max-w-xl">
